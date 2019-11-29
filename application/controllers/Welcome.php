2e6694126f107dded0e4d624b41d8083f9013160
anggitaaaaw <affebriany@gmail.com>
2019-11-29 20:19:21 +0700

    setting mongodb

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,7 +2,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
+
 	public function index(){
+		
 		$this->load->helper('url');
 		$this->load->view('login');
 	}
